03fe76e8cf9e56fabdaea7ac5e47511993d1baef
MoMMde <mommde@protonmail.com>
2022-07-16 17:40:48 +0200

    impl. image size filter -> see docs

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from threading import Thread
 from argparse import *
 from sys import argv
 from random import randint, choice
+from PIL import Image
 
 BASE_URL = 'https://i.imgur.com/'
 
 parser = ArgumentParser(description='Lets see what we can find...')
 parser.add_argument('--threads', '-T', type=int, default=8, help='How many Threads should be started')
 parser.add_argument('--directory', '-D', type=str, default='./output', help='Where all the Images should be stored')
+parser.add_argument('--minsize', '-M', type=str, default=None, help='Minimum size the Image can be')
 
 args = parser.parse_args()
 
@@ -21,7 +23,11 @@ def create_url(length: int) -> str:
     url += ''.join(choice(string.ascii_lowercase + string.digits) for _ in range(3))
     return url
 
-def crawl_pictures(thread: int, directory: str):
+def image_size(path: str) -> tuple:
+    image = Image.open(path)
+    return image.size
+
+def crawl_pictures(thread: int, directory: str, minX: int=0, minY: int=0):
     print(f'Initial thread execute: {thread}')
     while 1:
         url = create_url(randint(5, 6)) + '.jpg'
@@ -29,25 +35,39 @@ def crawl_pictures(thread: int, directory: str):
 
         response = requests.get(url, allow_redirects=False)
 
-        output_stream = open(f'{directory}/{filename}', 'wb')
+        image_dir = f'{directory}/{filename}'
+        output_stream = open(image_dir, 'wb')
         for chunk in response.iter_content(1024):
             output_stream.write(chunk)
         output_stream.close()
         
         if response.status_code in [404, 400, 302, 301, 300]:
             print(f'{thread} -> Invalid: {filename}')
-            os.remove(f'{directory}/{filename}')
+            os.remove(image_dir)
         else:
+            size = image_size(image_dir)
+            print(size)
+            print(size[0])
+            print(size[1])
+            print(size[0] < minX)
+            print(size[1] < minY)
+            if size[0] < minX or size[1] < minY:
+                print(f'{image_dir} -- Does not match minimum requirements. Deleting!')
+                os.remove(image_dir)
             print(f'{thread} -> Valid: {filename}')
+
 
 if args.threads == 0:
     print('Thread Count is 0, there won\'t be any crawling')
     exit(0)
 
 
+minX, minY = args.minsize.split('x')
+
+print(f'Minimum Size is: {minX}x{minY}')
 for i in range(args.threads):
     try:
-        thread = Thread(target=crawl_pictures, args=(i, args.directory))
+        thread = Thread(target=crawl_pictures, args=(i, args.directory, int(minX), int(minY),))
         thread.start()
     except:
         print(f'Couldn\'t start Thread: {i}')
