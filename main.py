ee9da77c46430626007892a0f9a2ed202188c96a
MoMMde <mommde@pm.me>
2022-08-22 01:15:25 +0200

    impl. ghost image verification & argument

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ from random import randint, choice
 from PIL import Image
 
 BASE_URL = 'https://i.imgur.com/'
+MEDIA_DATA_URL = 'https://api.imgur.com/post/v1/media/{id}?include=media%2Cadconfig%2Caccount'
 
 parser = ArgumentParser(description='Lets see what we can find...')
 parser.add_argument('--threads', '-T', type=int, default=8, help='How many Threads should be started')
 parser.add_argument('--directory', '-D', type=str, default='./output', help='Where all the Images should be stored')
 parser.add_argument('--minsize', '-M', type=str, default="100x100", help='Minimum size the Image can be | Enter by scaleX`x`scaleY as string. Example: 100x100')
 parser.add_argument('--id-length', '-L', type=str, default="6-7", help="Range or single value on how long the image id should be")
-
+parser.add_argument('--disallow-ghost', '-G', type=bool, default=False, help='If ghost images should be deleted')
 
 args = parser.parse_args()
 print(f'[ D ] Arguments: {args}')
@@ -27,18 +28,20 @@ def create_url() -> str:
         final_length = randint(int(from_range), int(to_range))
     else:
         final_length = int(args.id_length)
-    url = BASE_URL + ''.join(choice(string.ascii_letters + string.digits) for _ in range(final_length))
+    iid = ''.join(choice(string.ascii_letters + string.digits) for _ in range(final_length))
+    url = BASE_URL + iid
     #url += ''.join(choice(string.ascii_lowercase + string.digits) for _ in range(3))
-    return url
+    return (url, iid)
 
-def image_size(path: str) -> tuple:
-    image = Image.open(path)
-    return image.size
+def is_ghost_image(image_id: str):
+    response = requests.get(MEDIA_DATA_URL.replace("{id}", image_id))
+    return response.status_code == 404
 
-def crawl_pictures(thread: int, directory: str, minX: int=0, minY: int=0):
+def crawl_pictures(thread: int, directory: str, minX: int=0, minY: int=0, disallow_ghost: bool=False):
     print(f'[ T ] Created thread: {thread}')
     while 1:
-        url = create_url() + '.jpg'
+        url, iid = create_url()
+        url += '.jpg'
         filename = url.rsplit('/', 1)[-1]
 
         response = requests.get(url, allow_redirects=False)
@@ -52,12 +55,21 @@ def crawl_pictures(thread: int, directory: str, minX: int=0, minY: int=0):
             print(f'[ X ] Invalid code: {filename}')
             os.remove(image_dir)
         else:
-            size = image_size(image_dir)
+            image = Image.open(image_dir)
+            size = image.size
             if size[0] < minX or size[1] < minY:
                 print(f'[ E ] {image_dir} minimum requirements haven''t matched')
                 os.remove(image_dir)
             else:
                 print(f'[ I ] {filename} was found by thread {thread}')
+                if not is_ghost_image(iid):
+                    print(f'[ I ] {filename} is a normal image')
+                else:
+                    if disallow_ghost == True:
+                        os.remove(image_dir)
+                        print('[ W ] Deleting Ghost image...')
+                    print(f'[ W ] {filename} is a ghost image')
+
 
 
 if args.threads == 0:
@@ -70,7 +82,7 @@ minX, minY = args.minsize.split('x')
 print(f'Minimum Size is: {minX}x{minY}')
 for i in range(args.threads):
     try:
-        thread = Thread(target=crawl_pictures, args=(i, args.directory, int(minX), int(minY),))
+        thread = Thread(target=crawl_pictures, args=(i, args.directory, int(minX), int(minY), args.disallow_ghost))
         thread.start()
-    except:
+    except: 
         print(f'Couldn\'t start Thread: {i}')
